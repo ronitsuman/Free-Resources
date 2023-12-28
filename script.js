95e5992df3ce746d882a7ae4483cca598a32ba2a
Ronit Suman <ronitsuman59@gmail.com>
2023-12-28 21:37:32 +0530

    commit

---
+++ script.js
@@ -0,0 +1,33 @@
+// var a = 7 , b=3 ;
+// if (a%b === 0){
+//     console.log(a + "is divisible  ")
+// }
+// var a = 5 ;
+
+// if((a%1 === 0) && (a%2 === 0)) {
+//     console.log(" its prime no.")
+// }
+// else(
+//     console.log("number is not prime")
+// )
+// var num = 5;
+// for (var i=1; i<= num.length; i++){
+// var bag="";
+//    for(var j=1; j<= num.length; j++){
+//      bag+= j + " " // bag= bag+j --- old value +new value
+//    }
+//   console.log(bag)
+// }
+
+// var namenum = 5;
+// for(i=0; i<=nam ; i++){
+//     console.log(namenum)
+// }
+// var num = 9;
+// for (var i=1; i<=num; i++){
+// var bag= " " ;
+//    for(var j=1; j<=num; j++){
+//      bag+=j+"  " // bag= bag+j --- old value +new value
+//    }
+//   console.log(bag)
+// }
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,8 @@
+var num = 5;
+// for (var i=1; i<= num.length; i++){
+// var bag="";
+//    for(var j=1; j<= num.length; j++){
+//      bag+= j + " " // bag= bag+j --- old value +new value
+//    }
+//   console.log(bag)
+// }
